54ecfe0d19e49adfc408a2adbaec0b322f87d148
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-09-01 11:45:29 +0600

    added default display section

--- js/phero.js
+++ js/phero.js
@@ -29,23 +29,23 @@ const btnHandler = async (id) => {
     cardContainer.innerHTML = '';
 
     allCard.forEach(card => {
-        // console.log(card)
+        console.log(card)
         const singleCard = document.createElement('div');
         singleCard.classList = 'card bg-base-100 shadow-xl my-10 border p-5';
         singleCard.innerHTML = `
-        <figure><img src="${card.thumbnail}" alt="Shoes" /></figure> 
-                <div class="flex justify-between px-2 mt-5">
-                    <div class="w-[20%] h-[20%]">
-                        <img src=" ${card.authors[0].profile_picture} " class="rounded-full"/>
+        <figure><img src="${card.thumbnail}" /></figure> 
+                <div class="flex justify-between items-center px-2 mt-5">
+                    <div class="w-14">
+                        <img src=" ${card.authors[0].profile_picture}"
+                        class = "rounded-full"/>
                     </div>
                     <div class="pl-5 w-[80%]">
+                       <p class="text-xl font-bold my-3">${card.title}</p>
                         <div class="flex justify-between">
-                        <h1 class="text-xl font-bold">${card.authors[0].profile_name}</h1>
-                        <div>${card.authors[0].verified? '<span class ="bg-blue-500 text-xl px-3 py-1 rounded-full"><i class="fa-solid fa-check"></i></span>' : ""}</div>
+                        <h1 class="text-xl font-semibold">${card.authors[0].profile_name}</h1>
+                        <div>${card.authors[0].verified? '<span class ="bg-blue-600 text-2xl text-white px-3 py-1 rounded-full"><i class="fa-solid fa-check"></i></span>' : ""}</div>
                         </div>
-                        <h2 class="card-title">
-                        </h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 class="card-title"><span class="text-xl font-bold">Views:</span> ${card.others.views}</h2>
                       </div>
                 </div>
         `;
@@ -56,4 +56,6 @@ const btnHandler = async (id) => {
 
 }
 
+
 dataLoad();
+btnHandler('1001');
